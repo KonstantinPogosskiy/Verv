8dd84264552a3f2065f1e02054725b4561d4cadd
KonstantinPogosskiy <darcus666@yandex.ru>
2021-09-20 20:30:52 +0300

    refactor: add slider

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-let close = document.querySelector('.close');
+let theme = document.querySelector('.theme');
 let main = document.querySelector('.main');
 let column = document.querySelectorAll('.column');
 let list = document.querySelectorAll('.list');
+let slide = document.querySelectorAll('.slide');
+let next = document.querySelector('.slider-next');
+let prev = document.querySelector('.slider-prev');
 
-close.addEventListener('click', toggler);
+theme.addEventListener('click', toggler);
 
 function toggler() {
     main.classList.toggle('dark');
+
         for(let el of column) {
             el.classList.toggle('dark_theme');
         }
         for(let el of list) {
             el.classList.toggle('list_dark_theme');
         }
+        for(let el of slide) {
+            el.classList.toggle('slide-dark');
+        }
 }
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+next.addEventListener('click', () => {
+    offset += 300;
+    if(offset > 600) {
+        offset = 0
+    }
+    sliderLine.style.left = -offset + 'px';
+})
+
+prev.addEventListener('click', () => {
+    offset -= 300;
+    if(offset < 0) {
+        offset = 600
+    }
+    sliderLine.style.left = -offset + 'px';
+})
+
+(setInterval(() => {
+    offset += 300;
+    if(offset > 600) {
+        offset = 0
+    }
+    sliderLine.style.left = -offset + 'px';
+}, 5000))
