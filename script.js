d7ba552dc02a6e74571f378853b33e625d55146f
KonstantinPogosskiy <darcus666@yandex.ru>
2021-09-20 23:42:16 +0300

    refactor: add toggle

--- script.js
+++ script.js
@@ -5,46 +5,55 @@ let list = document.querySelectorAll('.list');
 let slide = document.querySelectorAll('.slide');
 let next = document.querySelector('.slider-next');
 let prev = document.querySelector('.slider-prev');
+let arrows = document.querySelector('.buttonCase');
 
 theme.addEventListener('click', toggler);
 
 function toggler() {
+    arrows.style.color = 'white';
+    if (arrows.style.color === 'black') {
+        arrows.style.color = 'white';
+    }
+    if (arrows.style.color === 'white') {
+        arrows.style.color = 'black';
+    }
+
 
     main.classList.toggle('dark');
 
-        for(let el of column) {
-            el.classList.toggle('dark_theme');
-        }
-        for(let el of list) {
-            el.classList.toggle('list_dark_theme');
-        }
-        for(let el of slide) {
-            el.classList.toggle('slide-dark');
-        }
+    for (let el of column) {
+        el.classList.toggle('dark_theme');
+    }
+    for (let el of list) {
+        el.classList.toggle('list_dark_theme');
+    }
+    for (let el of slide) {
+        el.classList.toggle('slide-dark');
+    }
 }
 
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 next.addEventListener('click', () => {
-    offset += 300;
-    if(offset > 600) {
+    offset += 370;
+    if (offset > 740) {
         offset = 0
     }
     sliderLine.style.left = -offset + 'px';
 })
 
 prev.addEventListener('click', () => {
-    offset -= 300;
-    if(offset < 0) {
-        offset = 600
+    offset -= 370;
+    if (offset < 0) {
+        offset = 740
     }
     sliderLine.style.left = -offset + 'px';
 })
 
 (setInterval(() => {
-    offset += 300;
-    if(offset > 600) {
+    offset += 370;
+    if (offset > 740) {
         offset = 0
     }
     sliderLine.style.left = -offset + 'px';
